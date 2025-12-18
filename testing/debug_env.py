67a94ff56a204ff55ada57498a9b9fa2c43b6675
AliZaki <aliazaki205@gmail.com>
2025-12-18 15:24:43 +0200

    Delete testing directory

--- testing/debug_env.py
+++
@@ -1,9 +0,0 @@
-# debug_env_path.py
-import os
-from dotenv import find_dotenv, load_dotenv
-
-path = find_dotenv()
-print("Loaded .env from:", path)
-
-load_dotenv()
-print("Loaded OPENROUTER_API_KEY:", os.getenv("OPENROUTER_API_KEY"))
--- testing/test.py
+++
@@ -1,3 +0,0 @@
-from llm_openrouter import generate_llm_answer
-
-print(generate_llm_answer("hello!"))
